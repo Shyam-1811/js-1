9b4cd2e00c3e3703e4bf37b0a2990f0deb674e34
Shyam-1811 <shyamverma1811@gmail.com>
2024-05-29 12:52:55 +0000

    js2

---
+++ jsbasics/datatypes.js
@@ -0,0 +1,14 @@
+let name = "Shyam"
+let rollno = 33
+let bool = false
+let state
+// data types 
+/*string = " "
+number/integer = 2**53 Around 
+BigInt
+Boolean
+null
+undefined
+Symbol -> used for uniqueness */
+console.table([typeof"Shyam",rollno,bool,undefined])
+// null is an object so its datatypes is not defined
---
+++ jsbasics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 123
+let email = "Shyam@google.com"
+var password = " Shyam"
+let state
+accountcity = "Jaipur"
+accountcity = "Biharsharif"
+email = "abc@gmail.com"
+password = " password"
+// accountId = 22
+console.log(accountId);
+console.table([accountcity,accountId,email,password,state])
+/*
+recommended not to use const keyword
+*/
